8649fa6875217ae08a2ea0e0b26504e46c6a1377
João Bibiano <jv.bibiano@hotmail.com>
2023-10-30 23:39:06 -0300

    add my own config, let's learn this

--- init.lua
+++ init.lua
@@ -227,7 +227,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -303,6 +303,11 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    oldfiles = {
+      cwd_only = true,
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
@@ -333,7 +338,8 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
+      'ruby' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,73 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+vim.o.relativenumber = true
+vim.o.shell = "/bin/zsh"
+
+vim.cmd('command! Gp !git push -u origin HEAD')
+vim.cmd('command! -nargs=1 Gcp !git fetch origin && git checkout <args> && git pull')
+vim.cmd('command! Gsc !stash_and_checkout_master.sh')
+
+vim.cmd('command! -nargs=1 Du !docker-compose up <args> -d')
+vim.cmd('command! -nargs=1 Dr !docker-compose restart <args>')
+
+vim.api.nvim_set_keymap('n', '<Leader>y', [[:lua CopyRelativePathToClipboard()<CR>]], { noremap = true, silent = true })
+
+function CopyRelativePathToClipboard()
+    local current_file_path = vim.fn.expand("%")
+    vim.fn.system('echo -n ' .. current_file_path .. ' | pbcopy')
+    print("Copied to clipboard: " .. current_file_path)
+end
+
+vim.api.nvim_set_keymap('n', '<Leader>fa', [[:lua LspFixAllProblems()<CR>]], { noremap = true, silent = true })
+
+function LspFixAllProblems()
+    vim.lsp.buf.code_action({
+        filter = function(a)
+		return a.title == "Fix all auto-fixable problems"
+	end,
+        apply = true
+    })
+end
+
+return {
+	{
+		'yuezk/vim-js',
+		'HerringtonDarkholme/yats.vim',
+		'maxmellon/vim-jsx-pretty',
+	},
+	{
+		'nvim-treesitter/nvim-treesitter-context',
+		config = function()
+			require 'treesitter-context'
+		end,
+	},
+	{
+		'github/copilot.vim',
+		config = function()
+			vim.api.nvim_set_keymap("i", "<C-C>", 'copilot#Accept("<CR>")', { silent = false, expr = true })
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
+		end,
+	},
+	{
+		'f-person/git-blame.nvim',
+		config = function()
+			vim.g.gitblame_date_format = '%r (%d %b %Y)'
+			vim.g.gitblame_message_template = '<author> • <date>'
+		end,
+	},
+	{
+		'windwp/nvim-autopairs',
+		event = "InsertEnter",
+		opts = {} -- this is equalent to setup({}) function
+	},
+	{
+		'tpope/vim-surround',
+	},
+	{
+		'christoomey/vim-tmux-navigator',
+		lazy = false,
+	}
+}
