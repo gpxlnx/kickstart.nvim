117442b441db7ebc35c2c715f20785ac90b174ce
João Bibiano <jv.bibiano@hotmail.com>
2024-05-05 06:03:31 -0300

    update theme & sync deeps

--- init.lua
+++ init.lua
@@ -191,11 +191,10 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
@@ -206,7 +205,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
